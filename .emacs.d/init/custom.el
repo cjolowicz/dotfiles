779956ba61ad60d6c7716e253de04b0695a86f7f
Claudio Jolowicz <cj@dealloc.org>
2013-11-23 18:49:43 +0100

    [emacs] Remove obsolete customizations.

--- .emacs.d/init/custom.el
+++ .emacs.d/init/custom.el
@@ -4,7 +4,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(line-move-visual nil))
+)
 
 ; Custom faces.
 (custom-set-faces
@@ -12,15 +12,4 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(diff-added ((t (:inherit diff-changed :foreground "yellow"))))
- '(diff-context ((((class color) (background light)) (:foreground "white"))))
- '(diff-function ((t (:inherit diff-context-face :foreground "white"))))
- '(diff-removed ((t (:inherit diff-changed :foreground "red"))))
- '(rst-level-1-face ((t (:background "cyan" :foreground "black"))) t)
- '(rst-level-2-face ((t (:background "cyan" :foreground "black"))) t)
- '(rst-level-3-face ((t (:background "cyan" :foreground "black"))) t)
- '(rst-level-4-face ((t (:background "cyan" :foreground "black"))) t)
- '(rst-level-5-face ((t (:background "cyan" :foreground "black"))) t)
- '(rst-level-6-face ((t (:background "cyan" :foreground "black"))) t)
- '(font-lock-comment-face ((((class color) (min-colors 8) (background dark))
-			    (:foreground "red")))))
+)
