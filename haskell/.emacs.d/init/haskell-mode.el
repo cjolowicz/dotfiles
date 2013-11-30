c5fbf4d9b465da28afe06cf0d10dc575dbbd99e0
Claudio Jolowicz <cj@dealloc.org>
2013-11-30 22:42:09 +0100

    [haskell] Remove emacs haskell-mode initialization.

--- haskell/.emacs.d/init/haskell-mode.el
+++
@@ -1,6 +0,0 @@
-; Load and configure haskell mode.
-(load "~/.emacs.d/lisp/haskell-mode/haskell-site-file")
-(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
-;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
-;;(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
