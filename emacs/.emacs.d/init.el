6c12b7b4929bee9003cb2f35e6fe23c1e1230768
Claudio Jolowicz <mail@claudiojolowicz.com>
2018-09-25 14:18:53 +0200

    [emacs] Remove $HOME/local/... from load path.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,9 +1,7 @@
 ; ~/.emacs: executed by emacs(1) on startup.
 
 ; Set the load path.
-(setq load-path (append '("~/local/share/emacs/site-lisp"
-			  "~/.emacs.d/lisp"
-			  ) load-path))
+(setq load-path (append '("~/.emacs.d/lisp") load-path))
 
 ; Set the custom file.
 (setq custom-file "~/.emacs.d/init/custom.el")
