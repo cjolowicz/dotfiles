0286726b809788e0331d25e6bd8a1fd631193417
Claudio Jolowicz <cjolowicz@cyren.com>
2018-10-08 12:07:39 +0200

    [emacs] Select packages yaml-mode and haskell-mode.

--- emacs/.emacs.d/init/custom.el
+++ emacs/.emacs.d/init/custom.el
@@ -4,6 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (yaml-mode haskell-mode)))
  '(standard-indent 4)
  '(tab-stop-list (quote (number-sequence 4 200 4))))
 
