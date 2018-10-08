6ea4158ac4a0d974e3eae25cb67ae5ff419a04bb
Claudio Jolowicz <cjolowicz@cyren.com>
2018-10-08 12:08:06 +0200

    [emacs] Customize colors used in diff-mode.

--- emacs/.emacs.d/init/diff-mode.el
+++ emacs/.emacs.d/init/diff-mode.el
@@ -1,1 +1,16 @@
 (add-to-list 'auto-mode-alist '("/patches/" . diff-mode))
+
+;; Customizing colors used in diff mode
+(defun custom-diff-colors ()
+  "update the colors for diff faces"
+  (set-face-attribute
+   'diff-context nil :foreground "white")
+  (set-face-attribute
+   'diff-header nil :foreground "white")
+  (set-face-attribute
+   'diff-added nil :foreground "green")
+  (set-face-attribute
+   'diff-removed nil :foreground "red")
+  (set-face-attribute
+   'diff-changed nil :foreground "purple"))
+(eval-after-load "diff-mode" '(custom-diff-colors))
