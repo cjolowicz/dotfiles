d2bebc942d14a938980c8f79af5afa5bffec87e0
Claudio Jolowicz <cjolowicz@cyren.com>
2018-10-08 12:07:11 +0200

    Customize diff syntax highlighting.

--- emacs/.emacs.d/init/custom.el
+++ emacs/.emacs.d/init/custom.el
@@ -13,4 +13,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(diff-context ((t (:foreground "white"))))
+ '(diff-file-header ((t (:background "brightblack" :weight bold))))
+ '(diff-header ((t (:background "brightblack")))))
