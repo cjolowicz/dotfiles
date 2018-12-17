677033a59c1303bf721566abf0b1e8fa3fa3b12b
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2018-12-17 13:49:13 +0100

    [emacs-js2-mode] Add extension .jsx, .json and .babelrc.

--- emacs-js2-mode/.emacs.d/init/js2-mode.el
+++ emacs-js2-mode/.emacs.d/init/js2-mode.el
@@ -1,5 +1,8 @@
 (require 'js2-mode)
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.babelrc\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.json\\'" . js2-mode))
 
 ;; Better imenu
 (add-hook 'js2-mode-hook #'js2-imenu-extras-mode)
