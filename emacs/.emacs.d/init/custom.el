0b8453642e3fc91a789bd31e0fef65a01c26a3ff
Claudio Jolowicz <mail@claudiojolowicz.com>
2019-01-10 15:16:06 +0100

    [emacs] Customize indent-tabs-mode

--- emacs/.emacs.d/init/custom.el
+++ emacs/.emacs.d/init/custom.el
@@ -4,6 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(indent-tabs-mode nil)
  '(js-indent-level 2)
  '(markdown-command "/usr/local/bin/pandoc")
  '(package-selected-packages
