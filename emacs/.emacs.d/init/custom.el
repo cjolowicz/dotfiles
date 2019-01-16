2a6adee01bea37c7686d5cbe57ae47256762bdab
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-01-16 20:05:16 +0100

    [emacs] Fix diff-mode colors

--- emacs/.emacs.d/init/custom.el
+++ emacs/.emacs.d/init/custom.el
@@ -12,13 +12,3 @@
     (toml-mode markdown-mode dockerfile-mode js2-mode haskell-mode jinja2-mode yaml-mode)))
  '(standard-indent 4)
  '(tab-stop-list (quote (number-sequence 4 200 4))))
-
-; Custom faces.
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(diff-context ((t (:foreground "white"))))
- '(diff-file-header ((t (:background "brightblack" :weight bold))))
- '(diff-header ((t (:background "brightblack")))))
--- emacs/.emacs.d/init/diff-mode.el
+++ emacs/.emacs.d/init/diff-mode.el
@@ -1,1 +1,23 @@
 (add-to-list 'auto-mode-alist '("/patches/" . diff-mode))
+
+(eval-after-load 'diff-mode
+  '(progn
+     (set-face-attribute 'diff-header nil
+                         :foreground "white" :background "brightblack")
+     (set-face-attribute 'diff-file-header nil
+                         :foreground "white" :background "brightblack")
+     (set-face-attribute 'diff-context nil
+                         :foreground "brightblack")
+     (set-face-attribute 'diff-added nil
+                         :foreground "darkgreen")
+     (set-face-attribute 'diff-removed nil
+                         :foreground "darkred")
+     (set-face-attribute 'diff-changed nil
+                         :foreground "darkblue")
+     (set-face-attribute 'diff-indicator-added nil
+                         :foreground "white" :background "darkgreen")
+     (set-face-attribute 'diff-indicator-removed nil
+                         :foreground "white" :background "darkred")
+     (set-face-attribute 'diff-indicator-changed nil
+                         :foreground "white" :background "darkblue")
+     ))
