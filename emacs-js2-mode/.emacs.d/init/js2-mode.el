61fe32424fa7e4ff76bdd3abb7d98e869e9c5a20
Claudio Jolowicz <mail@claudiojolowicz.com>
2019-02-01 20:09:02 +0100

    [emacs-js2-mode] Use js2-jsx-mode and flow-minor-mode

--- emacs-js2-mode/.emacs.d/init/js2-mode.el
+++ emacs-js2-mode/.emacs.d/init/js2-mode.el
@@ -1,8 +1,23 @@
-(require 'js2-mode)
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-(add-to-list 'auto-mode-alist '("\\.jsx\\'" . js2-mode))
-(add-to-list 'auto-mode-alist '("\\.babelrc\\'" . js2-mode))
-(add-to-list 'auto-mode-alist '("\\.json\\'" . js2-mode))
+(defun add-to-auto-mode-alist (mode patterns)
+  (dolist (pattern patterns)
+    (add-to-list 'auto-mode-alist
+                 (cons pattern mode))))
 
-;; Better imenu
-(add-hook 'js2-mode-hook #'js2-imenu-extras-mode)
+(require 'js2-mode)
+
+; Use js2-jsx-mode for Javascript files.
+(add-to-auto-mode-alist 'js2-jsx-mode
+ (list
+  "\\.js\\'"
+  "\\.jsx\\'"
+  "\\.babelrc\\'"
+  "\\.json\\'"
+  ))
+
+; Use js2-jsx-mode for scripts with a node shebang.
+(add-to-list 'interpreter-mode-alist '("node" . js2-jsx-mode))
+
+; Use flow-minor-mode when there is a //@flow declaration at the first
+; line of the file and a .flowconfig file is present in the project.
+; This requires https://github.com/an-sh/flow-minor-mode.
+(add-hook 'js2-mode-hook 'flow-minor-enable-automatically)
