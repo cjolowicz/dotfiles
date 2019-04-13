866c718acbe5b2012300a2bb5eb424063ff4d419
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-04-13 15:06:56 +0200

    [fish] Enhance d function

--- fish/.config/fish/functions/d.fish
+++ fish/.config/fish/functions/d.fish
@@ -1,4 +1,7 @@
-function d --description 'Simulate dirs -v'
-	printf "     0\t%s\n" (string replace -r '^'"$HOME"'($|/)' '~$1' -- $PWD)
-	string replace -r '^'"$HOME"'($|/)' '~$1' -- $dirstack | string join \n | cat -n;
+function d --description 'Simulate dirs -v and pushd +N' --argument n
+    if set -q n[1]
+        pushd +$n > /dev/null
+    end
+    printf "     0\t%s\n" (string replace -r '^'"$HOME"'($|/)' '~$1' -- $PWD)
+    string replace -r '^'"$HOME"'($|/)' '~$1' -- $dirstack | string join \n | cat -n;
 end
