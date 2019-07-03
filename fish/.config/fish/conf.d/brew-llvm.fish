4b09c2373bee623d75b8094dc7d094070ba1d6a3
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-07-03 11:59:12 +0200

    [fish] Set LDFLAGS and CPPFLAGS for LLVM

--- fish/.config/fish/conf.d/brew-llvm.fish
+++ fish/.config/fish/conf.d/brew-llvm.fish
@@ -1,9 +1,9 @@
 set -gx PATH /usr/local/opt/llvm/bin $PATH
 set -gx MANPATH /usr/local/opt/llvm/man ":$MANPATH"
+set -gx LDFLAGS "-L/usr/local/opt/llvm/lib -Wl,-rpath,/usr/local/opt/llvm/lib"
+set -gx CPPFLAGS "-I/usr/local/opt/llvm/include"
 # set -gx CC clang
 # set -gx CXX clang++
 # set -gx LD ld.lld
 # set -gx AR llvm-ar
 # set -gx RANLIB llvm-ranlib
-# set -gx LDFLAGS "-L/usr/local/opt/llvm/lib"
-# set -gx CPPFLAGS "-I/usr/local/opt/llvm/include"
