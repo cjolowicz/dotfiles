74c330f19bb1be7ff0ef8912dbe347d8d2e4d7ad
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-03-07 11:26:13 +0100

    [fish] Simplify poetry-env-activate using `poetry env info --path`

--- fish/.config/fish/functions/poetry-env-activate.fish
+++ fish/.config/fish/functions/poetry-env-activate.fish
@@ -1,3 +1,3 @@
 function poetry-env-activate --description "Activate the virtualenv of a Poetry-managed Python project"
-    source (dirname (poetry run which python))/activate.fish
+    source (poetry env info --path)/bin/activate.fish
 end
