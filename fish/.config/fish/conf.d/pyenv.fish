521d0071382303778d9c9e78e0332ca5d160114c
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-07-28 14:41:01 +0200

    [fish] Upgrade to Python 3.8.5 and 3.9.0b5

--- fish/.config/fish/conf.d/pyenv.fish
+++ fish/.config/fish/conf.d/pyenv.fish
@@ -1,9 +1,9 @@
 set -gx PATH ~/.pyenv/bin $PATH
 status --is-interactive; and source (pyenv init - | psub)
 pyenv global \
-    3.8.4 \
+    3.8.5 \
     2.7.18 \
     3.5.9 \
     3.6.11 \
     3.7.8 \
-    3.9.0b4
+    3.9.0b5
