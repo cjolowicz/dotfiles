2147e4bd5412359766f3bb61d46f6ac172b384e8
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-07-12 16:47:20 +0200

    [qutebrowser] Update

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -1,6 +1,8 @@
 # Load existing settings made via :set
 config.load_autoconfig()
 
-c.colors.webpage.prefers_color_scheme_dark = True
-c.statusbar.show = "never"
-c.tabs.show = "never"
+c.colors.webpage.preferred_color_scheme = "dark"
+#c.colors.webpage.darkmode.enabled = True
+c.tabs.show = "multiple"
+c.tabs.position = "bottom"
+#c.statusbar.show = "never"
