1377c1e2cf5a5082a50041c46906a170ddf916d2
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-08-27 10:54:02 +0200

    [qutebrowser] Configure

---
+++ qutebrowser/.qutebrowser/config.py
@@ -0,0 +1,8 @@
+# Load existing settings made via :set
+config.load_autoconfig()
+
+c.colors.webpage.preferred_color_scheme = "dark"
+#c.colors.webpage.darkmode.enabled = True
+c.tabs.show = "multiple"
+c.tabs.position = "bottom"
+#c.statusbar.show = "never"
